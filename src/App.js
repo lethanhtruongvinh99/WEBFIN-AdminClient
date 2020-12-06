9f5353c71ab2b42d10e8475595a0f71f8d7fe851
Phan Nhật Vinh <phannhatvinh10a@gmail.com>
2020-12-06 17:22:12 +0700

    Added login and register form

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import LoginForm from './containers/login-form/index';
+import RegisterForm from './containers/register-form/index'
 import
 {
   BrowserRouter as Router,
   Switch,
   Route,
+  Redirect,
   Link
 } from 'react-router-dom';
 
@@ -15,7 +17,11 @@ function App()
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={LoginForm} />
+        <Route exact path="/login" component={LoginForm} />
+        <Route exact path="/register" component={RegisterForm} />
+        <Route path="/" component={LoginForm}>
+          <Redirect to="/login" />
+        </Route>
       </Switch>
     </Router>
 
